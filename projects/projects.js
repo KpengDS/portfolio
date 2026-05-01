dcb6c39d1e2f67e424294dd72b7ad4c5a646c809
Kangxin Peng <k2peng@ucsd.edu>
2026-04-30 17:17:05 -0700

    s

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -8,3 +9,15 @@ renderProjects(projects, projectsContainer, 'h2');
 
 const title = document.querySelector('.projects-title');
 title.textContent = projects.length + ' Projects';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('#projects-pie-plot')
+  .append('path')
+  .attr('d', arc)
+  .attr('fill', 'red');
